ed56c2bd412f869611cfc1f4b0c758f481abfc7d
chiache <chiache7410@gmail.com>
2016-11-24 14:46:23 +0800

    2016-11-24

---
+++ app/src/main/java/tw/org/iii/lab15/MainActivity.java
@@ -0,0 +1,25 @@
+package tw.org.iii.lab15;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
+
+    }
+    public void sendData(View v){
+
+        myRef.setValue("Hello, World!" + (int)(Math.random()*100));
+    }
+}
